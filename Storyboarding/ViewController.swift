bebc088b36cfd5a5775ee626b70ce718d333a0a6
Stephen Ouyang <xisouyang@gmail.com>
2019-01-10 22:23:09 -0800

    Initial Commit

---
+++ Storyboarding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storyboarding
+//
+//  Created by Stephen Ouyang on 1/10/19.
+//  Copyright © 2019 Stephen Ouyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
